9dd89eb10d400fc2b3f7185135aaf14a2cd95f4e
DependencyInj <somdevhari.learning@gmail.com>
2024-07-05 21:56:19 +0530

    adding the local setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'gourmet/home', pathMatch:'full'
+  },
+  {
+    path: 'gourmet',
+    children:[
+      {
+        path:'home',
+        loadChildren: () => import('./features/table-home/table-home.module').then(m => m.TableHomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

d1280959ea77b3d98b929f1c55a7766cab1b4580
DependencyInj <somdevhari.learning@gmail.com>
2024-07-29 22:27:27 +0530

    style changes and additional form controls added to menu form

--- src/app/features/menu/components/add-menu-item/add-menu-item.component.ts
+++ src/app/features/menu/components/add-menu-item/add-menu-item.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
 import { MenuType } from '../../models/menu-type.model';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-menu-item',
@@ -8,24 +10,38 @@ import { MenuType } from '../../models/menu-type.model';
   styleUrls: ['./add-menu-item.component.scss']
 })
 export class AddMenuItemComponent {
+  readonly dialogRef = inject(MatDialogRef<AddMenuItemComponent>);
+  menuItemForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    desciption: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required]),
+    price: new FormControl(null, [Validators.required]),
+    url: new FormControl('', [Validators.required])
+  });
+
+
   value = '';
   selectedValue: any;
 
   menuTypes: MenuType[] = [];
 
-  constructor(private menuService: MenuService){
+  constructor(private menuService: MenuService) {
 
   }
 
-  ngOnInit(){
-    this,this.getMenuTypes();
+  ngOnInit() {
+    this, this.getMenuTypes();
   }
 
-  getMenuTypes(){
+  getMenuTypes() {
     this.menuService.getMenuTypes().subscribe({
       next: (resp) => {
         this.menuTypes = resp;
       }
     });
+  }
+
+  cancel() {
+    this.dialogRef.close();
   }
 }
